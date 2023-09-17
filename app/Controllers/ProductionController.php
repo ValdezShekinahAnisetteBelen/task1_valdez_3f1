f87a31d42fdba6198757e8cc845b7ccf2196a230
Valdez Shekinah Anisette Belen <shekinahvaldez2003@gmail.com>
2023-09-17 14:42:25 +0800

    continue

---
+++ app/Controllers/ProductionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class ProductionController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
